24e453ff29afc68955a8d2d47cfe8af3b235c26f
talent.he <talent.liang.he@accenture.com>
2021-06-07 22:09:15 +0800

    Add Hystrix

--- src/main/java/com/springboot/feign/client/EurekaClientFeign.kt
+++ src/main/java/com/springboot/feign/client/EurekaClientFeign.kt
@@ -4,6 +4,7 @@ import com.springboot.eureka.client.domain.Parent
 import com.springboot.eureka.client.domain.Student
 import com.springboot.feign.client.config.FeignClientConfig
 import com.springboot.feign.client.config.FeignClientInterceptorConfig
+import com.springboot.feign.client.hystrix.EurekaClientFeignHystrix
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -13,7 +14,8 @@ import org.springframework.web.bind.annotation.RequestParam
 // configuration 指定Feign 的配置
 // 如果不指定则会默认全局配置
 // Feign 可以创建多个配置
-@FeignClient(value = "eureka-client", configuration = [FeignClientConfig::class, FeignClientInterceptorConfig::class])
+@FeignClient(value = "eureka-client", configuration = [FeignClientConfig::class, FeignClientInterceptorConfig::class],
+                fallback = EurekaClientFeignHystrix::class)
 interface EurekaClientFeign {
 
     @GetMapping(value = ["/info"])
